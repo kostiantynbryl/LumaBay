db20786d76d8aca75f5ace88f021becd67bef853
Kostiantyn Bryl <kostiantynbryl@gmail.com>
2026-08-03 00:24:04 +0300

    fix: convert decorative shadow children into proper UI shadows

--- Assets/LumaBay/Runtime/Presentation/PanelShadowLayoutGuard.cs
+++ Assets/LumaBay/Runtime/Presentation/PanelShadowLayoutGuard.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,11 +6,13 @@ namespace LumaBay
 {
     public static class PanelShadowLayoutGuard
     {
+        private static readonly HashSet<int> Processed = new HashSet<int>();
         private static int lastFrame = -1;
 
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
         private static void Register()
         {
+            Processed.Clear();
             Canvas.willRenderCanvases -= Apply;
             Canvas.willRenderCanvases += Apply;
         }
@@ -22,10 +25,23 @@ namespace LumaBay
             RectTransform[] rects = Object.FindObjectsByType<RectTransform>(FindObjectsInactive.Include, FindObjectsSortMode.None);
             foreach (RectTransform rect in rects)
             {
-                if (rect.name != "PanelShadow") continue;
+                if (rect.name != "PanelShadow" || !Processed.Add(rect.GetInstanceID())) continue;
+
                 LayoutElement element = rect.GetComponent<LayoutElement>();
                 if (element == null) element = rect.gameObject.AddComponent<LayoutElement>();
                 element.ignoreLayout = true;
+
+                Image parentImage = rect.parent != null ? rect.parent.GetComponent<Image>() : null;
+                if (parentImage != null)
+                {
+                    Shadow shadow = parentImage.GetComponent<Shadow>();
+                    if (shadow == null) shadow = parentImage.gameObject.AddComponent<Shadow>();
+                    shadow.effectColor = new Color(0f, 0f, 0f, 0.48f);
+                    shadow.effectDistance = new Vector2(4f, -7f);
+                    shadow.useGraphicAlpha = true;
+                }
+
+                rect.gameObject.SetActive(false);
             }
         }
     }
